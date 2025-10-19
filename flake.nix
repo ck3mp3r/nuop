5fe92e8b2e818c902b2fa412a97a34502fca4ba2
Christian Kemper <christian.kemper@me.com>
2025-10-19 15:49:31 +0100

    refactor: migrate from flake-utils to flake-parts framework

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs";
     fenix = {
       url = "github:nix-community/fenix";
@@ -11,37 +11,34 @@
     };
   };
 
-  outputs = {
-    flake-utils,
-    fenix,
-    devshell,
-    nixpkgs,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [devshell.overlays.default];
-        pkgs = import nixpkgs {
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {system, ...}: let
+        overlays = [inputs.devshell.overlays.default];
+        pkgs = import inputs.nixpkgs {
           inherit system overlays;
         };
         operatorPackages = import ./operator/nix {
           inherit
-            fenix
-            nixpkgs
             overlays
             pkgs
             system
             ;
+          fenix = inputs.fenix;
+          nixpkgs = inputs.nixpkgs;
         };
       in {
         packages = operatorPackages;
+
         devShells.default = pkgs.devshell.mkShell {
           packages = [
             operatorPackages.toolchain
           ];
           imports = [
             (pkgs.devshell.importTOML ./devshell.toml)
-            "${devshell}/extra/git/hooks.nix"
+            "${inputs.devshell}/extra/git/hooks.nix"
           ];
           env = [
             {
@@ -52,6 +49,6 @@
         };
 
         formatter = pkgs.alejandra;
-      }
-    );
+      };
+    };
 }
