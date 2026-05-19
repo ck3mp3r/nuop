a187f1f4e3bd1a9f09ac1ad5fd34480bdb24c84c
Christian Kemper <christian.kemper@me.com>
2026-05-19 13:09:56 +0100

    chore: pin nixpkgs to nixpkgs-unstable in both flakes
    - Update root flake.nix to use nixpkgs-unstable
    - Update operator/flake.nix to use nixpkgs-unstable
    - Update both flake.lock files

--- operator/flake.nix
+++ operator/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
